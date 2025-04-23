98abbc1c56c0298ed8b781fea3c5a219b10308f0
L-droid815 <150846092+L-droid815@users.noreply.github.com>
2025-04-23 14:11:30 -0400

    En este archivo de inicio es donde comienza las variables y sus tipos

---
+++ index.py
@@ -0,0 +1,26 @@
+# Hola que tal a todos mi nombre es Luis Figuera soy Venezolano y estudiante de informatica
+# del primer trimestre y quiero compartirles un poco de mis conocimientos en python gracias
+# a lo que he podido aprender siendo autodidacta y capacitandome
+
+
+#Estas son las varibles
+
+String = "hola mundo"
+
+number = 12
+
+bool = False or True # (false = falso, verdadero = True)
+
+# Estos son los tipos de numeros que existen en python y en cualquier lenguaje
+# de programacion
+
+int = 14 # <- esto es un numero entero int significa integrer
+
+float = 15.5 # <- esto es un numero decimal float signifca numero decimal 
+
+# Nota: Nunca coloquen una coma si van a escribir un numero decimal porque la
+# computadora nunca lo va a reconocer es preferible colocar un punto siempre
+# en la programacion tienes que colocar un punto porque nunca el backend va
+# a reconocer que hay un float
+
+# Para mas informacion pueden contactarme a mi correo electronico lalejandrofiguera@gmail.com
