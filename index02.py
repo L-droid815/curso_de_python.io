e9bafc96cde0d801d71f6803c03cbdc0155c92a2
L-droid815 <150846092+L-droid815@users.noreply.github.com>
2025-04-24 21:28:50 -0400

    Condicionales

---
+++ index02.py
@@ -0,0 +1,36 @@
+# Esto son los condicionales if else y elif
+
+Edad = 18
+
+if Edad >= 18:
+    
+    print("Es mayor de edad y puede manejar")
+    
+else:
+    
+    print("No puede manejar porque no es mayor de edad")
+    
+    
+    
+# Este es un ejemplo usando el elif 
+
+nota = 20
+
+if nota == 20:
+    
+    print("Excelente")
+    
+elif nota >= 15:
+    
+    print("Muy bueno")
+    
+elif nota == 12:
+    
+    print("Pasaste por poco")
+    
+else:
+    
+    print("Reprobado")
+    
+    # Como pudieron ver el elif se puede repetir varias veces dependiendo de los ejercicios o los problemas que una empresa
+    # manda o puede mandar en un futuro
