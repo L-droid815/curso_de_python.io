bd2c40c0b0135853e94cf3ac4d049fcdd7b089df
L-droid815 <150846092+L-droid815@users.noreply.github.com>
2025-05-15 16:10:08 -0400

    Operadores ternarios

---
+++ index05.py
@@ -0,0 +1,11 @@
+# En la programación existe diferentes tipos de operadores ternarios y se puede
+# representar de diferentes formas pero en python una forma tan sencilla que
+# tiene que ver con los condicionales if elif else
+
+#por ejemplo:
+
+edad = 18
+
+Datos = "Adulto" if edad >= 18 else "No es adulto"
+
+print(Datos)
